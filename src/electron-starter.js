1c578bbd078e561815d4c469e191aae759b6a32e
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-11 17:34:06 -0700

    In dev, load create-react-app's localhost:3000, but in production, load built index.html

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 function createWindow() {
   // Create the browser window.
@@ -10,8 +12,13 @@ function createWindow() {
     },
   });
 
-  // and load the index.html of the app.
-  win.loadFile('index.html');
+  // In dev, load React app served by create-react-app.
+  // In production, load built index.html.
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  );
 
   // Open the DevTools.
   win.webContents.openDevTools();
