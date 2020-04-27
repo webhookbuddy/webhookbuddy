23d36f671aae1fce8599e65f230dba876caf18bd
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-27 10:41:34 -0700

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Dashboard from 'routes/Dashboard';
+import Endpoints from 'routes/Endpoints';
 
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <Router>
+      <Route exact path="/" component={Dashboard} />
+      <Route path="/endpoints" component={Endpoints} />
+    </Router>
+  );
 }
 
 export default App;
