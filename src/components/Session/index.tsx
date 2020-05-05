b409532045b2c4f75ea5d8c3e4a5999a24c42ff5
Johnny Oshika <johnnyoshika@gmail.com>
2020-05-05 14:39:56 -0700

    Add Session component

---
+++ src/components/Session/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+import { Me, useSetMe } from 'context/user-context';
+import { USER_FRAGMENT } from 'schema/fragments';
+import Error from 'components/Error';
+import Loading from 'components/Loading';
+
+export const GET_Me = gql`
+  query getMe {
+    me {
+      ...user
+    }
+  }
+  ${USER_FRAGMENT}
+`;
+
+export interface MePayload {
+  me: Me;
+}
+
+const Session = () => {
+  const setMe = useSetMe();
+  const { loading, error, refetch } = useQuery<MePayload>(GET_Me, {
+    notifyOnNetworkStatusChange: true,
+    onCompleted: ({ me }) => {
+      setMe(me);
+    },
+  });
+
+  const retry = () => refetch().catch(() => {}); // Unless we catch, a network error will cause an unhandled rejection: https://github.com/apollographql/apollo-client/issues/3963
+
+  if (error)
+    return (
+      <Error error={error}>
+        <button className="btn btn-primary" onClick={retry}>
+          Try again!
+        </button>
+      </Error>
+    );
+
+  if (loading) return <Loading />;
+
+  return <div>Almost ready...</div>;
+};
+
+export default Session;
