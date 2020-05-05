ee7dee9b86e80387e884a67268287e7ad23f259f
Johnny Oshika <johnnyoshika@gmail.com>
2020-05-05 14:57:17 -0700

    Copy MutationFunctionOptions type from 'react-apollo' which we're not using
    Alternative is to 'npm install --save react-apollo', but that seems overkill just to reference a type in Typescript

---
+++ src/schema/types.ts
@@ -0,0 +1,30 @@
+/*
+Copied this from `react-apollo` to get `MutationFunctionOptions` for type referencing in useMutation
+*/
+
+import {
+  PureQueryOptions,
+  OperationVariables,
+  MutationUpdaterFn,
+  WatchQueryFetchPolicy,
+} from 'apollo-client';
+
+export declare type Context = Record<string, any>;
+export declare type RefetchQueriesFunction = (
+  ...args: any[]
+) => Array<string | PureQueryOptions>;
+
+export interface MutationFunctionOptions<
+  TData = any,
+  TVariables = OperationVariables
+> {
+  variables?: TVariables;
+  optimisticResponse?: TData | ((vars: TVariables | {}) => TData);
+  refetchQueries?:
+    | Array<string | PureQueryOptions>
+    | RefetchQueriesFunction;
+  awaitRefetchQueries?: boolean;
+  update?: MutationUpdaterFn<TData>;
+  context?: Context;
+  fetchPolicy?: WatchQueryFetchPolicy;
+}
