a5e717e0805c2c8ab4598bb41ca740760143642d
Johnny Oshika <johnnyoshika@gmail.com>
2020-05-13 15:00:47 -0700

    Render property endpoint name

--- src/routes/Dashboard/Header/Endpoint/index.tsx
+++ src/routes/Dashboard/Header/Endpoint/index.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { useApolloClient } from '@apollo/react-hooks';
+import { Endpoint as EndpointType } from 'schema/types';
 
 import './style.css';
+import { ENDPOINT_FRAGMENT } from 'schema/fragments';
 
 const Endpoint = () => {
+  const {
+    endpointId,
+  }: {
+    endpointId: string;
+  } = useParams();
+
+  const client = useApolloClient();
+  const endpoint = client.readFragment<EndpointType>(
+    {
+      id: `Endpoint:${endpointId}`,
+      fragment: ENDPOINT_FRAGMENT,
+      fragmentName: 'endpoint',
+    },
+    true,
+  );
+
   return (
     <div className="dash-header__endpoint">
       <Link to="/">
         <i className="dash-header__endpoint__icon fa fa-exchange fa-2x"></i>
         <span className="dash-header__endpoint__title">
-          ACME w/ Stripe
+          {endpoint?.name}
         </span>
       </Link>
     </div>
