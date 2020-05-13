bdd096a15adb43360a3357c8ac491e8bb2ddf174
Johnny Oshika <johnnyoshika@gmail.com>
2020-05-13 11:01:38 -0700

    Switch to HashRouter so that routing works when running from static files
    https://stackoverflow.com/a/50404777/188740

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 import { useMe } from 'context/user-context';
