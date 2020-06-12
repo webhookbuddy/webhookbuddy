ce2edeff3bf99e833138c083c21f727ca765c210
Johnny Oshika <johnnyoshika@gmail.com>
2020-06-12 08:45:58 -0700

    Only show Back link in endpoints if there is a useful 'back' route

--- src/routes/Endpoints/Header/index.tsx
+++ src/routes/Endpoints/Header/index.tsx
@@ -5,9 +5,16 @@ import './style.css';
 
 const Header = () => {
   const history = useHistory();
+
+  // history.length 2 seems to be the default for both HashRouter and BrowserRouter
+
   return (
     <div className="ep-header" onClick={() => history.goBack()}>
-      <div className="ep-header__content">
+      <div
+        className={`ep-header__content ${
+          history.length < 3 ? 'invisible' : 'visible'
+        }`}
+      >
         <span className="pointer">
           <i className="ep-header__icon fa fa-arrow-left fa-2x"></i>
           <span className="ep-header__title">Back</span>
