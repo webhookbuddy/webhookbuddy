eae7ece29c489004096844955343738512754eae
Johnny Oshika <johnnyoshika@gmail.com>
2020-07-15 14:16:54 -0700

    Add FilterEnum

--- src/routes/Dashboard/Sidebar/index.tsx
+++ src/routes/Dashboard/Sidebar/index.tsx
@@ -4,6 +4,15 @@ import Webhooks from './Webhooks';
 
 import './style.css';
 
+export enum FilterEnum {
+  All = 'All',
+  Unread = 'Unread',
+  Read = 'Read',
+  Unforwarded = 'Unforwarded',
+  Forwarded = 'Forwarded',
+  Last24Hours = 'Last 24 hours',
+}
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
