fa2a97ec13c203dfbef80d74e0714ec26027a1a3
Johnny Oshika <johnnyoshika@gmail.com>
2020-09-08 12:27:55 -0700

    Rename Main/Some/Body component Main/Some/Content
    Made this change b/c we introduced another Body component higher up in the hierarchy

---
+++ src/routes/Dashboard/Body/Main/Some/Content/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Request from './Request';
+import Forwards from './Forwards';
+import { Webhook } from 'schema/types';
+
+import './style.css';
+
+const Content = ({ webhook }: { webhook: Webhook }) => {
+  return (
+    <div className="main__content">
+      <Route
+        exact
+        path={`/dashboard/:endpointId/webhooks/:webhookIds`}
+        render={props => <Request {...props} webhook={webhook} />}
+      />
+      <Route
+        path={`/dashboard/:endpointId/webhooks/:webhookIds/forwards`}
+        render={props => <Forwards {...props} webhook={webhook} />}
+      />
+    </div>
+  );
+};
+
+export default Content;
