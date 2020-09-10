3538527ed685d747cb1576693554711ba83c0697
Johnny Oshika <johnnyoshika@gmail.com>
2020-09-10 14:10:26 -0700

    Convert Main

--- src/routes/Dashboard/Body/Main/index.tsx
+++ src/routes/Dashboard/Body/Main/index.tsx
@@ -3,11 +3,11 @@ import { Route } from 'react-router-dom';
 import None from './None';
 import Some from './Some';
 
-import './style.css';
+import styles from './styles.module.css';
 
 const Main = () => {
   return (
-    <div className="main">
+    <div className={styles.main}>
       <Route exact path="/dashboard/:endpointId" component={None} />
       <Route
         path="/dashboard/:endpointId/webhooks/:webhookIds"
