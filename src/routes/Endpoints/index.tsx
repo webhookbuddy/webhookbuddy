5e63d331292a86762b20d74deba74fb38edd1ed1
Johnny Oshika <johnnyoshika@gmail.com>
2020-09-10 11:52:44 -0700

    Convert Endpoints

--- src/routes/Endpoints/index.tsx
+++ src/routes/Endpoints/index.tsx
@@ -4,15 +4,15 @@ import Header from './Header';
 import List from './List';
 import Create from './Create';
 
-import './style.css';
+import styles from './styles.module.css';
 
 const Endpoints = () => {
   return (
-    <div className="viewport viewport--endpoints">
+    <div className={styles.viewport}>
       <Header />
-      <div className="endpoints">
-        <div className="endpoints__container">
-          <div className="endpoints__container__pad">
+      <div className={styles.endpoints}>
+        <div className={styles.endpointsFullHeight}>
+          <div className={styles.endpointsPadding}>
             <div className="container">
               <Route exact path="/" component={List} />
               <Route path="/endpoints/create" component={Create} />
