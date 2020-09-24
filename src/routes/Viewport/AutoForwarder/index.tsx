7cbe2cb2723f28e34c16330dacb1a954385c04fb
Johnny Oshika <johnnyoshika@gmail.com>
2020-09-24 14:20:58 -0700

    Populate endpoints in AutoForwarder

--- src/routes/Viewport/AutoForwarder/index.tsx
+++ src/routes/Viewport/AutoForwarder/index.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_ENDPOINTS } from 'schema/queries';
+import { GetEndpoints } from 'schema/types/GetEndpoints';
+import Error from 'components/Error';
 
 import styles from './styles.module.css';
 
 const AutoForwarder = ({ docked }: { docked: Boolean }) => {
+  const { data, error, loading, refetch } = useQuery<GetEndpoints>(
+    GET_ENDPOINTS,
+    {
+      fetchPolicy: 'cache-and-network',
+    },
+  );
+
+  const [endpointId, setEndpointId] = useState('');
+
+  const retry = () => refetch().catch(() => {}); // Unless we catch, a network error will cause an unhandled rejection: https://github.com/apollographql/apollo-client/issues/3963
+
   return (
     <div
       className={`${styles.autoForwarder} ${
@@ -16,7 +31,32 @@ const AutoForwarder = ({ docked }: { docked: Boolean }) => {
       </div>
       <div className="form-group">
         <label>Endpoint</label>
-        <select className="custom-select custom-select-sm"></select>
+        {error ? (
+          <div style={{ fontSize: '10px' }}>
+            <Error error="">
+              <button
+                className="btn btn-primary btn-sm"
+                onClick={retry}
+              >
+                Try again!
+              </button>
+            </Error>
+          </div>
+        ) : (
+          <select
+            className="custom-select custom-select-sm"
+            onChange={e => setEndpointId(e.target.value)}
+            value={endpointId}
+          >
+            {data &&
+              !loading &&
+              data?.endpoints.map(endpoint => (
+                <option key={endpoint.id} value={endpoint.id}>
+                  {endpoint.name}
+                </option>
+              ))}
+          </select>
+        )}
       </div>
       <div className="form-group">
         <label>Auto-forward to</label>
