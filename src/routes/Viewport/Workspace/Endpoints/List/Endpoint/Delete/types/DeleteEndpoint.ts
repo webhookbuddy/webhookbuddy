a01edcc26e76005f603bfc8ef77e2db548f1da11
Johnny Oshika <johnnyoshika@gmail.com>
2021-01-16 16:36:08 -0800

    Move codegen types to new folder structure

--- src/routes/Viewport/Workspace/Endpoints/List/Endpoint/Delete/types/DeleteEndpoint.ts
+++ src/routes/Viewport/Workspace/Endpoints/List/Endpoint/Delete/types/DeleteEndpoint.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { DeleteEndpointInput } from "./../../../../../../types/global-types";
+import { DeleteEndpointInput } from "./../../../../../../../../types/global-types";
 
 // ====================================================
 // GraphQL mutation operation: DeleteEndpoint
