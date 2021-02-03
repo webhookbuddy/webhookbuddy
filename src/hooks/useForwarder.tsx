b4990dcc5616a3e5ab11bb141bccdedb22a939bd
Johnny Oshika <johnnyoshika@gmail.com>
2021-02-02 23:02:27 -0800

    Refactor Node dependent request into useNodeSender

--- src/hooks/useForwarder.tsx
+++ src/hooks/useForwarder.tsx
@@ -1,130 +1,67 @@
-import { Webhook, KeyValue } from 'schema/types';
-import { useEffect } from 'react';
+import { Webhook } from 'schema/types';
 import useForwardingIds from './useForwardingIds';
 import useForwardUrls from './useForwardUrls';
-import useReadWebhook from './useReadWebhook';
-import { useMe } from 'context/user-context';
 import {
   AddForward_addForward_webhook,
   AddForward_addForward_webhook_forwards,
 } from './types/AddForward';
 import useAddForward from './useAddForward';
-import {
-  appendQuery,
-  extractContentType,
-  mapHeaders,
-} from 'utils/http-fragment';
-
-const { ipcRenderer } = window.require('electron');
+import { appendQuery } from 'utils/http-fragment';
+import useSender from './useNodeSender';
 
 const useForwarder = (endpointId: string) => {
-  const me = useMe();
   const { addForwardingIds, removeForwardingId } = useForwardingIds();
   const { addForwardUrl } = useForwardUrls(endpointId);
   const { addForward } = useAddForward();
-  const { readWebhook } = useReadWebhook();
 
-  useEffect(() => {
-    const onForwardedListener = (
-      _: any,
-      {
-        metadata,
-        rawHeaders,
-        statusCode,
-        data,
-        error,
-      }: {
-        metadata: {
-          url: string;
-          webhook: AddForward_addForward_webhook;
-        };
-        statusCode: number;
-        rawHeaders: string[];
-        data: string;
-        error: any;
+  const onForwarded = (
+    webhook: AddForward_addForward_webhook,
+    forward: AddForward_addForward_webhook_forwards,
+  ) => {
+    removeForwardingId(webhook.id);
+
+    addForward({
+      variables: {
+        input: {
+          webhookId: webhook.id,
+          url: forward.url,
+          method: forward.method,
+          statusCode: forward.statusCode,
+          // need to remap here b/c server rejects __typename property
+          headers: forward.headers.map(kv => ({
+            key: kv.key,
+            value: kv.value,
+          })),
+          // need to remap here b/c server rejects __typename property
+          query: forward.query.map(kv => ({
+            key: kv.key,
+            value: kv.value,
+          })),
+          body: forward.body,
+        },
       },
-    ) => {
-      const forward = {
-        __typename: 'Forward',
-        id: '_' + Math.round(Math.random() * 1000000),
-        url: metadata.url,
-        statusCode: error ? 502 : statusCode,
-        success: statusCode >= 200 && statusCode < 300,
-        createdAt: new Date(),
-        method: metadata.webhook.method,
-        headers: mapHeaders(rawHeaders),
-        query: metadata.webhook.query,
-        contentType: extractContentType(mapHeaders(rawHeaders)),
-        body: data ?? '',
-        user: me,
-      } as AddForward_addForward_webhook_forwards;
-
-      removeForwardingId(metadata.webhook.id);
-
-      addForward({
-        variables: {
-          input: {
-            webhookId: metadata.webhook.id,
-            url: forward.url,
-            method: forward.method,
-            statusCode: forward.statusCode,
-            // need to remap here b/c server rejects __typename property
-            headers: forward.headers.map(kv => ({
-              key: kv.key,
-              value: kv.value,
-            })),
-            // need to remap here b/c server rejects __typename property
-            query: forward.query.map(kv => ({
-              key: kv.key,
-              value: kv.value,
-            })),
-            body: forward.body,
+      optimisticResponse: {
+        addForward: {
+          __typename: 'AddForwardPayload',
+          webhook: {
+            ...webhook,
+            forwards: [forward, ...webhook.forwards],
           },
         },
-        optimisticResponse: {
-          addForward: {
-            __typename: 'AddForwardPayload',
-            webhook: {
-              ...metadata.webhook,
-              forwards: [forward, ...metadata.webhook.forwards],
-            },
-          },
-        },
-      });
-    };
+      },
+    });
+  };
 
-    ipcRenderer.on('http-request-completed', onForwardedListener);
-
-    return () => {
-      ipcRenderer.removeListener(
-        'http-request-completed',
-        onForwardedListener,
-      );
-    };
-  }, [removeForwardingId, addForward, readWebhook, me]);
+  const { send } = useSender({ onForwarded });
 
   const forwardWebhook = (url: string, webhooks: Webhook[]) => {
     addForwardUrl(url);
     addForwardingIds(webhooks.map(w => w.id));
     webhooks
       .sort((a, b) => parseInt(a.id, 10) - parseInt(b.id, 10))
-      .forEach(webhook => {
-        ipcRenderer.send('http-request', {
-          method: webhook.method,
-          url: appendQuery(url, webhook.query),
-          headers: webhook.headers
-            .filter(header => header.key.toLowerCase() !== 'host')
-            .reduce((acc, cur) => {
-              acc[cur.key] = cur.value;
-              return acc;
-            }, {} as any),
-          body: webhook.body,
-          metadata: {
-            url,
-            webhook,
-          },
-        });
-      });
+      .forEach(webhook =>
+        send(appendQuery(url, webhook.query), webhook),
+      );
   };
 
   return {
---
+++ src/hooks/useNodeSender.tsx
@@ -0,0 +1,89 @@
+import { useMe } from 'context/user-context';
+import { useEffect } from 'react';
+import { Webhook } from 'schema/types';
+import { extractContentType, mapHeaders } from 'utils/http-fragment';
+import {
+  AddForward_addForward_webhook,
+  AddForward_addForward_webhook_forwards,
+} from './types/AddForward';
+
+const { ipcRenderer } = window.require('electron');
+
+const useNodeSender = ({
+  onForwarded,
+}: {
+  onForwarded: (
+    webhook: AddForward_addForward_webhook,
+    forward: AddForward_addForward_webhook_forwards,
+  ) => void;
+}) => {
+  const me = useMe();
+
+  useEffect(() => {
+    const onForwardedListener = (
+      _: any,
+      {
+        metadata,
+        rawHeaders,
+        statusCode,
+        data,
+        error,
+      }: {
+        metadata: {
+          url: string;
+          webhook: AddForward_addForward_webhook;
+        };
+        statusCode: number;
+        rawHeaders: string[];
+        data: string;
+        error: any;
+      },
+    ) => {
+      onForwarded(metadata.webhook, {
+        __typename: 'Forward',
+        id: '_' + Math.round(Math.random() * 1000000),
+        url: metadata.url,
+        statusCode: error ? 502 : statusCode,
+        success: statusCode >= 200 && statusCode < 300,
+        createdAt: new Date(),
+        method: metadata.webhook.method,
+        headers: mapHeaders(rawHeaders),
+        query: metadata.webhook.query,
+        contentType: extractContentType(mapHeaders(rawHeaders)),
+        body: data ?? '',
+        user: me,
+      } as AddForward_addForward_webhook_forwards);
+    };
+
+    ipcRenderer.on('http-request-completed', onForwardedListener);
+
+    return () => {
+      ipcRenderer.removeListener(
+        'http-request-completed',
+        onForwardedListener,
+      );
+    };
+  }, [me]);
+
+  const send = (url: string, webhook: Webhook) => {
+    ipcRenderer.send('http-request', {
+      method: webhook.method,
+      url,
+      headers: webhook.headers
+        .filter(header => header.key.toLowerCase() !== 'host')
+        .reduce((acc, cur) => {
+          acc[cur.key] = cur.value;
+          return acc;
+        }, {} as any),
+      body: webhook.body,
+      metadata: {
+        url,
+        webhook,
+      },
+    });
+  };
+
+  return { send };
+};
+
+export default useNodeSender;
