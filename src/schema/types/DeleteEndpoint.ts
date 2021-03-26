067ce9f7bdee8306463a4ae87fb4feb5769beb6c
buzzbumble <s.kang919@gmail.com>
2021-03-26 12:41:28 -0700

    Regenerated types for queries in /schema;

---
+++ src/schema/types/DeleteEndpoint.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { DeleteEndpointInput } from "./../../types/global-types";
+
+// ====================================================
+// GraphQL mutation operation: DeleteEndpoint
+// ====================================================
+
+export interface DeleteEndpoint_deleteEndpoint {
+  __typename: "DeleteEndpointPayload";
+  affectedRows: number;
+}
+
+export interface DeleteEndpoint {
+  deleteEndpoint: DeleteEndpoint_deleteEndpoint;
+}
+
+export interface DeleteEndpointVariables {
+  input: DeleteEndpointInput;
+}
---
+++ src/schema/types/Login.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { LoginInput } from "./../../types/global-types";
+
+// ====================================================
+// GraphQL mutation operation: Login
+// ====================================================
+
+export interface Login_login {
+  __typename: "Token";
+  token: string;
+}
+
+export interface Login {
+  login: Login_login;
+}
+
+export interface LoginVariables {
+  input: LoginInput;
+}
