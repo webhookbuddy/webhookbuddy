82fac8035f8c9a67a95b6f3c69da565b2c818c82
TrungYU <quoctrung1220@gmail.com>
2021-04-02 09:45:28 -0700

    updated error and loading diplay for registration page

--- src/components/Register/RegisterForm/index.tsx
+++ src/components/Register/RegisterForm/index.tsx
@@ -1,9 +1,8 @@
 import { FormEvent, useState } from 'react';
-import Loading from 'components/Loading';
-import Error from 'components/Error';
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
-
+import Loading from 'components/Loading';
+import Error from 'components/Error';
 import { ApolloError, MutationFunctionOptions } from '@apollo/client';
 import { ExecutionResult } from 'graphql';
 import {
@@ -55,127 +54,132 @@ const RegisterForm = ({
     <div className={styles.container}>
       <div className="gutter" />
       <div className={styles.main}>
-        <form onSubmit={handleSubmit}>
-          <div className={`form-group ${styles.formGroupCustom}`}>
-            <label
-              className={
-                firstName
-                  ? `${styles.floatLabel}`
-                  : `${styles.noLabel}`
-              }
+        {error && <Error error={error} />}
+        {loading ? (
+          <Loading />
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <div className={`form-group ${styles.formGroupCustom}`}>
+              <label
+                className={
+                  firstName
+                    ? `${styles.floatLabel}`
+                    : `${styles.noLabel}`
+                }
+              >
+                {' '}
+                First Name:
+              </label>
+              <input
+                type="text"
+                value={firstName}
+                placeholder="First Name"
+                onChange={e => setFirstName(e.target.value)}
+                className="form-control"
+                required
+                autoFocus
+              />
+            </div>
+            <div className={`form-group ${styles.formGroupCustom}`}>
+              <label
+                className={
+                  lastName
+                    ? `${styles.floatLabel}`
+                    : `${styles.noLabel}`
+                }
+              >
+                {' '}
+                Last Name:
+              </label>
+              <input
+                type="text"
+                value={lastName}
+                placeholder="Last Name"
+                onChange={e => setLastName(e.target.value)}
+                className="form-control"
+                required
+              />
+            </div>
+            <div className={`form-group ${styles.formGroupCustom}`}>
+              <label
+                className={
+                  email ? `${styles.floatLabel}` : `${styles.noLabel}`
+                }
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                value={email}
+                placeholder="Email"
+                onChange={e => setEmail(e.target.value)}
+                className="form-control"
+                required
+              />
+            </div>
+            <div className={`form-group ${styles.formGroupCustom}`}>
+              <label
+                className={
+                  password
+                    ? `${styles.floatLabel}`
+                    : `${styles.noLabel}`
+                }
+              >
+                {' '}
+                Password:
+              </label>
+              <input
+                type="password"
+                value={password}
+                placeholder="Password"
+                onChange={e => setPassword(e.target.value)}
+                className="form-control"
+                required
+              />
+            </div>
+            <div className={`form-group ${styles.formGroupCustom}`}>
+              <label
+                className={
+                  confirmPassword
+                    ? `${styles.floatLabel}`
+                    : `${styles.noLabel}`
+                }
+              >
+                Confirm Password:
+              </label>
+              <label
+                style={{
+                  opacity:
+                    password &&
+                    confirmPassword &&
+                    password !== confirmPassword
+                      ? 1
+                      : 0,
+                  color: 'red',
+                }}
+              >
+                {' '}
+                Password do not match{' '}
+              </label>
+              <input
+                type="password"
+                value={confirmPassword}
+                placeholder="Confirm Password"
+                onChange={e => setConfirmPassword(e.target.value)}
+                className="form-control"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary btn-block mt-4"
             >
-              {' '}
-              First Name:
-            </label>
-            <input
-              type="text"
-              value={firstName}
-              placeholder="First Name"
-              onChange={e => setFirstName(e.target.value)}
-              className="form-control"
-              required
-              autoFocus
-            />
-          </div>
-          <div className={`form-group ${styles.formGroupCustom}`}>
-            <label
-              className={
-                lastName
-                  ? `${styles.floatLabel}`
-                  : `${styles.noLabel}`
-              }
-            >
-              {' '}
-              Last Name:
-            </label>
-            <input
-              type="text"
-              value={lastName}
-              placeholder="Last Name"
-              onChange={e => setLastName(e.target.value)}
-              className="form-control"
-              required
-            />
-          </div>
-          <div className={`form-group ${styles.formGroupCustom}`}>
-            <label
-              className={
-                email ? `${styles.floatLabel}` : `${styles.noLabel}`
-              }
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              value={email}
-              placeholder="Email"
-              onChange={e => setEmail(e.target.value)}
-              className="form-control"
-              required
-            />
-          </div>
-          <div className={`form-group ${styles.formGroupCustom}`}>
-            <label
-              className={
-                password
-                  ? `${styles.floatLabel}`
-                  : `${styles.noLabel}`
-              }
-            >
-              {' '}
-              Password:
-            </label>
-            <input
-              type="password"
-              value={password}
-              placeholder="Password"
-              onChange={e => setPassword(e.target.value)}
-              className="form-control"
-              required
-            />
-          </div>
-          <div className={`form-group ${styles.formGroupCustom}`}>
-            <label
-              className={
-                confirmPassword
-                  ? `${styles.floatLabel}`
-                  : `${styles.noLabel}`
-              }
-            >
-              Confirm Password:
-            </label>
-            <label
-              style={{
-                opacity:
-                  password &&
-                  confirmPassword &&
-                  password !== confirmPassword
-                    ? 1
-                    : 0,
-                color: 'red',
-              }}
-            >
-              {' '}
-              Password do not match{' '}
-            </label>
-            <input
-              type="password"
-              value={confirmPassword}
-              placeholder="Confirm Password"
-              onChange={e => setConfirmPassword(e.target.value)}
-              className="form-control"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="btn btn-primary btn-block mt-4"
-          >
-            Register
-          </button>
-          <br />
-          Already have an account? <Link to="/">Login</Link>!
-        </form>
+              Register
+            </button>
+            <br />
+            Already have an account? <Link to="/">Login</Link>!
+          </form>
+        )}
       </div>
       <div className="gutter" />
     </div>
