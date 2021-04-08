a7c4b9d3150464dba4da1ade5eb74510b9c01d02
buzzbumble <s.kang919@gmail.com>
2021-04-07 20:18:02 -0700

    Moved REGISTER_USER query to schema/queries; Regenerated type; Removed type from components/Register;

--- src/schema/types/Register.ts
+++ src/schema/types/Register.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { RegisterInput } from "./../../../types/global-types";
+import { RegisterInput } from "./../../types/global-types";
 
 // ====================================================
 // GraphQL mutation operation: Register
