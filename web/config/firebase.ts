c0a30ec11ff3ac5721f1f925b1922f4a157340be
Johnny Oshika <johnnyoshika@gmail.com>
2022-01-03 21:07:35 -0800

    fixup! Install and configure firebase-admin

--- web/config/firebase.ts
+++ web/config/firebase.ts
@@ -2,6 +2,7 @@ import * as admin from 'firebase-admin';
 
 // initializeApp looks for service account credentials in GOOGLE_APPLICATION_CREDENTIALS environment variable
 // and determines which Firebase project we're using
-admin.initializeApp();
+if (!admin.apps.length) admin.initializeApp();
 
 export const db = admin.firestore();
+export const functionsUrl = process.env.FIREBASE_FUNCTIONS_URL;
